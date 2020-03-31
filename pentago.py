adb64a90e4374483ada3d32910b1d281c4848a14
Francesco Mistri <francesco.mistri98@gmail.com>
2020-03-31 15:29:06 +0200

    Docstrings

--- pentago.py
+++ pentago.py
@@ -10,12 +10,21 @@ COUNTCLOCKWISE = False
 SECTION_SIZE = 3
 
 class Pentago:
+    """ Model class for the game state. """
+
     def __init__(self):
+        """ Contruct a new pentago game model. """
         self.matrix = []
         for i in range(6):
             self.matrix.append([BLANK, BLANK, BLANK, BLANK, BLANK, BLANK])
 
     def place(self, color, point):
+        """ Place a pawn on a given point.
+
+        `point` is in the form (row, colum)
+        `color` is BLACK or WHITE
+        Return True if success, False else
+        """
         if self.matrix[point[0]][point[1]] != BLANK:
             return False
 
@@ -23,6 +32,11 @@ class Pentago:
         return True
 
     def rotate(self, section, verse):
+        """Rotate one section of the board.
+
+        `section` is in the form (row, column)
+        `verse` is CLOCKWISE or COUNTCLOCKWISE
+        """
         result = copy.deepcopy(self.matrix)
         for i in range(section[0] * SECTION_SIZE, section[0] * SECTION_SIZE + SECTION_SIZE):
             for j in range(section[1] * SECTION_SIZE, section[1] * SECTION_SIZE + SECTION_SIZE):
