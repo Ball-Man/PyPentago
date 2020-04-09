841c7540d237cee3d9175164dbfcf7cfe698a7e2
Francesco Mistri <francesco.mistri98@gmail.com>
2020-04-09 22:41:47 +0200

    Console game: matrix visualization

--- main.py
+++ main.py
@@ -1,8 +1,92 @@
+import model.turn_controller as turn_controller
+import model.pentago as pentago
 
+from os import system, name as sysname
+from termcolor import colored
+
+
+CMD = '>> '
+QUIT_CMD = 'quit'
+NEW_GAME = 'new_game'
+
+hor_test = '─/┼/╬/═/║'
+
+PAWN = 'O'
+VERT_SEP = '|'
+HOR_SEP = '='
+SPACE = ' '
+
+
+COLORS = { pentago.WHITE: 'white', pentago.BLACK: 'red' }
+
+
+def read_input():
+    inp = input(CMD)
+    return inp.strip().lower()
+
+
+def clear():
+    if sysname == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
+def hor_line():
+    output = ''
+    for i in range(2 + pentago.MATRIX_SIZE * 2 + 1):
+        output += HOR_SEP
+    output += '\n'
+    return output
+
+def section_hor_line():
+    output = '|'
+    for i in range(1 + pentago.MATRIX_SIZE * 2):
+        output += HOR_SEP
+    output += '|\n'
+    return output
+
+def print_matrix(matrix):
+    output = ''
+
+    output += hor_line()
+
+    i = 0
+    while i < pentago.MATRIX_SIZE:
+        output += VERT_SEP + SPACE
+
+        for j in range(len(matrix[i])):
+            cell = matrix[i][j]
+
+            output += (colored(PAWN, COLORS[cell])
+                       if cell != pentago.BLANK else ' ') + ' '
+
+        output += VERT_SEP + '\n'
+
+        if (i + 1) % pentago.SECTION_SIZE == 0 and i + 1 != pentago.MATRIX_SIZE:
+            output += section_hor_line()
+
+        i += 1
+
+    output += hor_line()
+    print(output)
 
 def main():
-    pass
+    # Init
+    clear()
+    inp = ''
 
+    # Pentago model
+    game = turn_controller.TurnController()
+    for i in range(6):
+        for j in range(6):
+            game.do_turn((i, j), (0, 0), pentago.CLOCKWISE)
+
+    # Game loop
+    while inp != QUIT_CMD:
+        print_matrix(game.game_matrix)
+        inp = read_input()
+        clear()
 
 if __name__ == '__main__':
     main()
